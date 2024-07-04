afef1fbabab48f631dd40f8088f8f7dae7524e4a
Zishan Shaikh <zishan@truelink.ai>
2024-07-04 14:59:36 +0530

    first commit

--- app/src/main/java/com/example/androidprac/MainActivity.kt
+++ app/src/main/java/com/example/androidprac/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.androidprac.androidpractice.ButtonComp
 import com.example.androidprac.ui.theme.AndroidPracTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +22,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidPracTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(Modifier.padding(innerPadding)) {
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidPracTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/androidprac/androidpractice/ButtonComp.kt
@@ -0,0 +1,59 @@
+package com.example.androidprac.androidpractice
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+enum class ButtonType{
+    DEFAULT,
+    PRESSED,
+    DISABLED
+}
+
+@Composable
+fun ButtonComp(
+    Text: String,
+    onClick: () -> Unit,
+    modifier: Modifier,
+    fontFamily: FontFamily,
+    fontSize: Int,
+    fontWeight: FontWeight,
+    type: ButtonType,
+    color: Color,
+    contentColor: Color,
+
+
+){
+    Button(onClick = onClick,
+        modifier = Modifier
+        .width(147.dp)
+        .height(48.dp)
+        .background(color = Color.Yellow, shape = RoundedCornerShape(size = 8.dp))
+        .padding(start = 20.dp, top = 12.dp, end = 20.dp, bottom = 12.dp)) {
+        Text(
+            text = Text,
+            fontFamily = fontFamily,
+            fontSize = fontSize.sp,
+            fontWeight = fontWeight
+        )
+        if (type == ButtonType.PRESSED) {
+            Text(
+                text = Text,
+                fontFamily = fontFamily,
+                fontSize = fontSize.sp,
+                fontWeight = fontWeight
+            )
+        }
+    }
+}
