0554dec14556759becff66d4240974f384d1c50f
Zishan Shaikh <zishan@truelink.ai>
2024-07-04 19:09:14 +0530

    Created a Reusable Button

--- app/src/main/java/com/example/androidprac/MainActivity.kt
+++ app/src/main/java/com/example/androidprac/MainActivity.kt
@@ -8,11 +8,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidprac.androidpractice.ButtonComp
+import com.example.androidprac.androidpractice.MainScreen
 import com.example.androidprac.ui.theme.AndroidPracTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +20,7 @@ class MainActivity : ComponentActivity() {
             AndroidPracTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(Modifier.padding(innerPadding)) {
-
+                        MainScreen()
                     }
                 }
             }
--- app/src/main/java/com/example/androidprac/androidpractice/ButtonComp.kt
+++ app/src/main/java/com/example/androidprac/androidpractice/ButtonComp.kt
@@ -1,63 +1,99 @@
 package com.example.androidprac.androidpractice
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-//enum class for different types of button
+// Enum class for different types of buttons
 enum class ButtonType {
-    DEFAULT,
     PRESSED,
-    DISABLED
+    DISABLED,
+    DEFAULT
 }
 
-//Reusable function button
+@Composable
+fun MainScreen() {
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        ButtonComp(
+            text = "Button",
+            onClick = {},
+            type = ButtonType.DEFAULT,
+            style = TextStyle(fontSize = 16.sp, color = Color.Black)
+        )
+    }
+}
+
+// Reusable Button Component Function with default values
 @Composable
 fun ButtonComp(
-    Text: String,
+    text: String,
     onClick: () -> Unit,
-    modifier: Modifier,
-    fontFamily: FontFamily,
-    fontSize: Int,
-    fontWeight: FontWeight,
-    type: ButtonType,
-    color: Color,
-    contentColor: Color,
+    modifier: Modifier = Modifier,
+    fontSize: TextUnit = 16.sp,
+    type: ButtonType = ButtonType.DISABLED,
+    shape: RoundedCornerShape = RoundedCornerShape(16.dp),
+    style: TextStyle = TextStyle(fontSize = 16.sp, color = Color.Black)
+) {
+    // use when statement to set different button colors based on types
+    val buttonColors = when (type) {
+        ButtonType.PRESSED -> ButtonDefaults.buttonColors(
+            contentColor = Color.White,
+            containerColor = Color.Yellow
+        )
 
+        ButtonType.DISABLED -> ButtonDefaults.buttonColors(
+            contentColor = Color.Gray,
+            containerColor = Color.Gray
+        )
 
-    ) {
+        ButtonType.DEFAULT -> ButtonDefaults.buttonColors(
+            contentColor = Color.White,
+            containerColor = Color.Blue
+        )
+    }
+
+    // Button with default values
     Button(
         onClick = onClick,
-        modifier = Modifier
+        modifier = modifier
             .width(147.dp)
-            .height(48.dp)
-            .background(color = Color.Yellow, shape = RoundedCornerShape(size = 8.dp))
-            .padding(start = 20.dp, top = 12.dp, end = 20.dp, bottom = 12.dp)
+            .height(48.dp),
+        shape = shape,
+        colors = buttonColors
     ) {
-        Text(
-            text = Text,
-            fontFamily = fontFamily,
-            fontSize = fontSize.sp,
-            fontWeight = fontWeight
-        )
-        if (type == ButtonType.PRESSED) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(imageVector = Icons.Default.KeyboardArrowLeft, contentDescription = "Arrowleft")
             Text(
-                text = Text,
-                fontFamily = fontFamily,
-                fontSize = fontSize.sp,
-                fontWeight = fontWeight
+                text = text,
+                fontSize = fontSize,
+                style = TextStyle(color = Color.White)
             )
+            Image(imageVector = Icons.Default.KeyboardArrowRight, contentDescription = "Arrowright")
         }
     }
 }
