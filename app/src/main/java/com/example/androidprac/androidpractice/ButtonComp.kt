fc48bdebf74fac382ef45315f4cbc7c204ed1087
Zishan Shaikh <zishan@truelink.ai/in>
2024-07-06 14:56:19 +0530

    New Changes Reusable Button

--- app/src/main/java/com/example/androidprac/androidpractice/ButtonComp.kt
+++ app/src/main/java/com/example/androidprac/androidpractice/ButtonComp.kt
@@ -1,5 +1,6 @@
 package com.example.androidprac.androidpractice
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -7,6 +8,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
@@ -16,7 +20,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -35,11 +38,27 @@ fun MainScreen() {
     )
     {
         ButtonComp(
+            text = "New",
+            modifier = Modifier
+                .width(148.dp)
+                .height(48.dp),
             onClick = { },
             shape = RoundedCornerShape(16.dp),
             enabled = true,
             style = TextStyle(Color.White),
-            colors = ButtonDefaults.buttonColors(color.tertiary)
+            colors = ButtonDefaults.buttonColors(color.tertiary),
+            IconStart = {
+                Image(
+                    imageVector = Icons.Default.KeyboardArrowLeft,
+                    contentDescription = "backarrow"
+                )
+            },
+            IconEnd = {
+                Image(
+                    imageVector = Icons.Default.KeyboardArrowRight,
+                    contentDescription = "End Icon"
+                )
+            }
         )
     }
 }
@@ -50,10 +69,9 @@ fun ButtonComp(
     text: String = "Button",
     onClick: () -> Unit,
     enabled: Boolean = true,
-    fontSize: TextUnit = 16.sp,
     contentPadding: PaddingValues = PaddingValues(
-        start = 16.dp,
-        end = 16.dp,
+        start = 14.dp,
+        end = 14.dp,
         top = 0.dp,
         bottom = 0.dp
     ),
@@ -62,16 +80,15 @@ fun ButtonComp(
     style: TextStyle = TextStyle(fontSize = 16.sp, color = Color.Black),
     IconStart: @Composable (() -> Unit)? = null,
     IconEnd: @Composable (() -> Unit)? = null,
-    colors: ButtonColors = ButtonDefaults.buttonColors(color.primary),
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
 ) {
     Button(
         onClick = onClick,
         enabled = enabled,
-        modifier = modifier
-            .width(147.dp)
-            .height(48.dp),
+        modifier = modifier,
         shape = shape,
-        colors = colors
+        colors = colors,
+        contentPadding = contentPadding
     )
     {
         Row(
@@ -83,7 +100,7 @@ fun ButtonComp(
             }
             Text(
                 text = text,
-                style = TextStyle(fontSize = fontSize, color = Color.Red),
+                style = style
             )
             if (IconEnd != null) {
                 IconEnd()
