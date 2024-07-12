1c33773b479f64c1e51b75e94a2d03c3958a17ab
Zishan Shaikh <zishan@truelink.ai/in>
2024-07-12 12:27:09 +0530

    Custom Input Field Updated

--- app/src/main/java/com/example/androidprac/MainActivity.kt
+++ app/src/main/java/com/example/androidprac/MainActivity.kt
@@ -4,19 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.androidprac.androidpractice.CustomInputField
+import com.example.androidprac.androidpractice.MyCustomInputField
 import com.example.androidprac.ui.theme.AndroidPracTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,20 +21,7 @@ class MainActivity : ComponentActivity() {
             AndroidPracTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(Modifier.padding(innerPadding)) {
-                        //NavController()
-                        CustomInputField(value = "",
-                            onValueChange = {},
-                            modifier = Modifier.padding(16.dp),
-                            shape = RoundedCornerShape(16.dp),
-                            placeholder = { Text(text = "Enter Your Name") },
-                            keyboardActions = KeyboardActions.Default,
-                            leadingIcon = {
-                                Image(
-                                    imageVector = Icons.Default.AccountCircle,
-                                    contentDescription = ""
-                                )
-                            }
-                        )
+                        MyCustomInputField()
                     }
                 }
             }
--- app/src/main/java/com/example/androidprac/androidpractice/InputFieldPage.kt
+++ app/src/main/java/com/example/androidprac/androidpractice/InputFieldPage.kt
@@ -1,38 +1,84 @@
 package com.example.androidprac.androidpractice
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun Any.CustomInputField(
-    value: String,
-    onValueChange: (String) -> Unit,
-    placeholder: @Composable() (() -> Unit)? = null,
-    leadingIcon: @Composable() (() -> Unit)? = null,
-    trailingIcon: @Composable() (() -> Unit)? = null,
-    label: @Composable() (() -> Unit)? = null,
-    modifier: Modifier,
-    shape: Shape = OutlinedTextFieldDefaults.shape,
-    colors: TextFieldColors = OutlinedTextFieldDefaults.colors(),
+fun CustomInputField(
+    value: TextFieldValue,
+    onValueChange: (TextFieldValue) -> Unit,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    singleLine: Boolean = false,
+    modifier: Modifier = Modifier,
+    shape: Shape = MaterialTheme.shapes.small,
+    colors: TextFieldColors = TextFieldDefaults.colors(),
     keyboardActions: KeyboardActions = KeyboardActions.Default,
 ) {
-    OutlinedTextField(
+    val interactionSource = remember { MutableInteractionSource() }
+
+    TextField(
         value = value,
         onValueChange = onValueChange,
+        label = label,
         placeholder = placeholder,
         leadingIcon = leadingIcon,
         trailingIcon = trailingIcon,
-        label = label,
-        modifier = Modifier,
+        visualTransformation = visualTransformation,
+        enabled = enabled,
+        readOnly = readOnly,
+        singleLine = singleLine,
+        modifier = modifier
+            .padding(top = if (label != null) 5.dp else 0.dp),
         shape = shape,
         colors = colors,
-        keyboardActions = keyboardActions
+        keyboardActions = keyboardActions,
+        interactionSource = interactionSource,
     )
-
 }
+
+@Composable
+fun MyCustomInputField() {
+    var text by remember { mutableStateOf(TextFieldValue()) }
+
+    CustomInputField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        label = { Text("Email") },
+        placeholder = { Text("johnexample@gmail.com") },
+        leadingIcon = { Icon(Icons.Filled.Email, contentDescription = "emailIcon") },
+        trailingIcon = { Icon(Icons.Filled.Check, contentDescription = "checkIcon") },
+        singleLine = true,
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
 
